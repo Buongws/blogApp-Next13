2319873ad5068f62addf541bceeb333e7fd11f82
Tiendao <clonedopdz@gmail.com>
2023-09-23 20:08:39 +0700

    continue fix bug

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,17 @@
+"use client";
+import { signIn, useSession } from "next-auth/react";
 import styles from "./login.module.css";
 
 const LoginPage = () => {
+  const { data, status } = useSession();
+  console.log(status);
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sign in with Google</div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
